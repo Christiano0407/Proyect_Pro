fa3fb86ee7c717b09d8df4778c60917d3b700067
Christiano0407 <placebo0704@gmail.com>
2020-11-03 12:57:59 -0600

    callbacks

---
+++ src/async/callbacks.js
@@ -0,0 +1,13 @@
+//Callback dentro de callback
+/* function soyAsincrono(miCallback) {
+    setTimeout(function() {
+    console.log("Soy ASincronismo")
+    miCallback();
+    },1000)
+}
+soyAsincrono(function() {
+    console.log("Hola Callback");
+});
+console.log("Yo soy un Callback"); */
+
+//2)
