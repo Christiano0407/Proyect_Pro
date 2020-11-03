786c84c441061fdc866b264625fb9cac97e7f25d
Christiano0407 <placebo0704@gmail.com>
2020-11-03 12:04:54 -0600

    variables de entorno

---
+++ src/js/index.js
@@ -0,0 +1,16 @@
+/* console.log("Hola Mundo"); */
+//Monohilo = ERROR = Parar todo el proceso. 
+/* let i = 0;
+setInterval(function() {
+    console.log(i);
+    i++;
+}, 1000); */
+
+//Variables de Entorno.
+//Puedes trabajarlas en node=terminal
+//nombre=Chris node src/js/index.js
+//Buena práctica con mayúsculas (NOMBRE / WEB).
+/* let nombre = process.env.NOMBRE || `Sin Nombre`;
+let web = process.env.WEB || `No tiene web`;
+console.log(`Hola` + " " + nombre);
+console.log(`Mi Sitio Web es ` + " " + web); */
