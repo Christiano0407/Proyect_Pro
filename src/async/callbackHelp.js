e4cb8301c574a748f032547cc7505b41eac21078
Christiano0407 <placebo0704@gmail.com>
2020-11-04 08:30:36 -0600

    callbacks-help

---
+++ src/async/callbackHelp.js
@@ -0,0 +1,20 @@
+function hola(nombre, miCallback) {
+    setTimeout(function() {
+    console.log(`Hola, `+ nombre);
+    miCallback(nombre);
+}, 1000);
+}
+function adios(nombre, otroCallback) {
+    setTimeout(function() {
+    console.log(`Adios`, nombre);
+    otroCallback();
+    }, 1000)
+}
+
+//--
+console.log(`Iniciando Proceso...`);
+hola(`Carlos`, function(nombre) {
+    adios(nombre, function() {
+        console.log(`Terminando el proceso...`)
+    });
+});
