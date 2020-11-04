f7f6a7a7fe8aa68d8e7d39911414066fe7875cc8
Christiano0407 <placebo0704@gmail.com>
2020-11-04 09:24:10 -0600

    callbackHelp

--- src/async/callbackHelp.js
+++ src/async/callbackHelp.js
@@ -8,13 +8,37 @@ function adios(nombre, otroCallback) {
     setTimeout(function() {
     console.log(`Adios`, nombre);
     otroCallback();
-    }, 1000)
+    }, 1500)
+}
+
+function hablar(callbackHablar) {
+    setTimeout(function() {
+      console.log("Vamos a hablar");
+    callbackHablar();
+    }, 1200);
 }
 
 //--
-console.log(`Iniciando Proceso...`);
-hola(`Carlos`, function(nombre) {
-    adios(nombre, function() {
-        console.log(`Terminando el proceso...`)
-    });
+function conversacion(nombre, veces, callback) {
+if(veces > 0) {
+    hablar(function() {
+     conversacion(nombre, --veces, callback);
+    })
+}else {
+    adios(nombre, callback)
+}
+}
+console.log("Iniciando proceso");
+hola("Carlos", function(nombre) {
+conversacion(nombre, 3, function() {
+    console.log("Proceso terminao");
 });
+})
+/* console.log(`Iniciando Proceso...`);
+hola(`Carlos`, function(nombre) {
+    hablar(function() {
+        adios(nombre, function() {
+            console.log(`Terminando el proceso...`)
+        });
+    })
+}); */
