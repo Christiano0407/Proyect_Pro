bd7e496e2253d9f2d0bc8aa373b40a4785c4475d
Christiano0407 <placebo0704@gmail.com>
2020-11-05 08:01:43 -0600

    archivostxt

--- src/modulos/failSistem.js
+++ src/modulos/failSistem.js
@@ -1,15 +1,15 @@
 //FS = Acceder a archivos de nuestro sistema y editarlos / Bases de Datos.
 
-const fs = require(`fs`);
+/* const fs = require(`fs`);
 
 function leer(ruta, cb) {
     fs.readFile(ruta, (err, data) => {
     console.log(data.toString());
     })
-}
+} */
 
 //--Escribir
-function escribir(ruta, contenido,cb) {
+/* function escribir(ruta, contenido,cb) {
     fs.writeFile(ruta, contenido, function(err) {
      if(err) {
          console.log("No he podido escribirlo", err);
@@ -17,7 +17,16 @@ function escribir(ruta, contenido,cb) {
          console.log("Se ha escrito correctamente.")
      }
     })
-}
+} */
 
-escribir(__dirname + `/archivo01.txt`, `Soy un archivo Nuevo`, console.log);
+/* escribir(__dirname + `/archivo01.txt`, `Soy un archivo Nuevo`, console.log); */
 //leer(__dirname + `/archivo.txt`, console.log);
+
+//Promesa
+/* const fs = require(`fs`).promises;
+
+const readFile = async () => {
+    const file = await fs.readFile(`/archivo.txt`, {encoding: `utf-8`});
+    console.log(file);
+}
+readFile();  */
