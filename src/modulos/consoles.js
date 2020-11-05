959df2042039845d539a5961ff693598fd57ca6a
Christiano0407 <placebo0704@gmail.com>
2020-11-05 08:51:29 -0600

    consoles

---
+++ src/modulos/consoles.js
@@ -0,0 +1,30 @@
+//.log() --Permite verlo dentro del naveghador.
+
+//.info() = Información.
+//.error() = Percatarnos que existe un error.
+/* console.error(`Mal!`); */
+//.warn() = Equivalente  a log, pero, es usado para warning.
+//.table() = Muestra una tabla a partir de un objeto
+/* let tabla = [
+    {
+      name: "Chris",
+      edad: 28
+    },
+    {
+      city: "CDMX",
+      country: "México"
+    }
+] */
+//console.log(tabla);
+//console.table(tabla);
+//.group() = Agrupar / groupEnd() = Termina el grupo.
+/* console.group(`conver`);
+console.log("Hola");
+console.log("Hola2");
+console.log("Hola3");
+console.groupEnd(`conver`); */
+//count() = Contador.
+/* console.count(`veces`);
+console.count(`veces`);
+console.count(`veces`);
+console.count(`veces`); */
